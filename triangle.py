5dd05f6ed4993b5c16dde40b2bcc22c5a77e25f8
AkashAdarsh <42816603+AkashAdarsh@users.noreply.github.com>
2022-09-26 20:46:31 -0400

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,37 +1,32 @@
-# -*- coding: utf-8 -*-
+#!/user_id/bin/env python
+
 """
-
-The primary goal of this file is to demonstrate a simple python program to classify triangles
-
-@author:Akash Adarsh
+Triangle.py
 """
+__author__ = "Akash Adarsh"
 
 
-def classify_triangle(side_a, side_b, side_c):
-    """
-    Classify Triangle
-    """
-    # requires input values be >= 0 and <= 200
-    # all three should be int
-    instance_list = [isinstance(side_a, int), isinstance(side_b, int), isinstance(side_c, int)]
-    if (max(side_a, side_b, side_c) > 200 or
-            min(side_a, side_b, side_c) < 0 or
-            not instance_list):
+def classifyTriangle(a, b, c):
+
+    # verify that all 3 inputs are integers
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
         return 'InvalidInput'
 
-    # this is an extra implementation to give a more accurate output
-    if ((side_a >= (side_b + side_c)) or
-            (side_b >= (side_a + side_c)) or
-            (side_c >= (side_a + side_b))):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+
+    # Check for Triangle Property : The sum of the length of the two sides
+    # of a triangle is greater than the length of the third side.
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
 
-    # now we know that we have a valid triangle
-    if side_a == side_b and side_b == side_c:
+    # After All checks above, We can Classify the Triangles
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((((side_a ** 2) + (side_b ** 2)) == (side_c ** 2)) or
-          (((side_a ** 2) + (side_c ** 2)) == (side_b ** 2)) or
-          (((side_c ** 2) + (side_b ** 2)) == (side_a ** 2))):
+    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((b ** 2) + (c ** 2)) == (a ** 2):
         return 'Right'
-    elif (side_a != side_b) and (side_b != side_c) and (side_a != side_c):
+    elif (a != b) and (b != c) and (a != c):
         return 'Scalene'
-    return 'Isosceles'
+    else:
+        return 'Isosceles'
